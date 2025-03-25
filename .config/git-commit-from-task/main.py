ee9a18b2d2698c52bcb8dbc690b3546097bd4c4f
Dawid Roszman <dawidroszman@protonmail.com>
2025-03-25 13:17:31 +0100

    fix pr name for not fish shells

--- .config/git-commit-from-task/main.py
+++ .config/git-commit-from-task/main.py
@@ -11,10 +11,10 @@ def convert_to_commit(task: list[str]) -> str:
     return " ".join(task).replace(" ", "-").replace("[", "").replace("]","").lower()
 
 def convert_to_pr(task: list[str]) -> str:
-    return task[0].replace("[", "").replace("]", "")+": "+" ".join(task[1:])
+    return task[0].replace("[", "").replace("]", "")+" ".join(task[1:])
 
 branch = user_input[1]+"/"+convert_to_commit(user_input[2:])
-pr_name = user_input[1].upper()+"-"+convert_to_pr(user_input[2:])
+pr_name = (user_input[1].upper()+"-"+convert_to_pr(user_input[2:])).replace(" ", ": ", 1)
 pyperclip.copy(branch)
 print(branch)
 print(pr_name)
